fb937aeda12ab2cd3630a1a463339fcf3f1dd6e6
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-29 21:23:23 +0530

    Create 1Operations.r

---
+++ 1Operations.r
@@ -0,0 +1,78 @@
+#Variables in R 
+
+##Assigning Variables 
+x = 12
+y<-"Hello"
+x
+y
+TRUE ->B
+B
+
+###################
+#Operators 
+
+##Arithmetic Operators 
+2+3 #add
+3-1 #subtraction
+2*3 #multiplication
+10/5 #division
+9^2 #power 
+5%%2 #remainder
+7%/%3 #quotient
+7/3
+
+################
+#assignment 
+x=5
+x
+x<-165
+x
+25->x
+x
+x<<-30
+x
+58->>x
+x
+
+###################
+##Relational operators 
+2>3
+3>2
+4+5>2
+31<5*6
+24<5*9
+3*3==9
+3*2==5
+34<=30+4
+2!=3
+2!=2
+
+##################
+#Logical Operators 
+2&3 
+(2+3)&(3+2)
+(2+3)|(3+2)
+!(2+3)
+x=TRUE
+y=FALSE
+x&y
+#returns x if x is false 
+#otherwise y 
+y&x
+x&X
+x&x
+y&y
+x|y
+#returns y if x is false 
+#otherwise it returns x
+!x
+
+!y
+########################
+##special operators 
+x=2:5
+x
+y<-5
+y%in%x
+
+
