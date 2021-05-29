6c2691ae84e7309a6a08dab4fb5948b460a87d56
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-29 21:24:57 +0530

    Create 3List.r

---
+++ 3List.r
@@ -0,0 +1,31 @@
+#LIST 
+n=c(2,3,5)
+n
+m=c("aa","bb")
+m
+x=list(n,s)
+x
+class(x)
+class(m)
+class(n)
+y=list(m,n,vtr)
+y
+
+list1<-list("JK",12,3,TRUE)
+list1
+list2<-list("R",14,3,FALSE)
+list2
+list3=merge(list1,list2)
+list3
+list4=list(m,"JK",1)
+list4
+class(list3)
+list2[2]
+list7=list("a",1,2,"b")
+list8=list("c",3,4,"d")
+list9=merge(list7,list8)
+class(list9)
+M <- matrix(1:12, 3, 4)
+M
+M <- matrix(1:12, ncol = 4)
+M
