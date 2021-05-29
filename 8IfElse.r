5c4cbff6f784e06bd3ea718fe28c25d7c21261f6
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-29 21:43:42 +0530

    Create 8IfElse.r

---
+++ 8IfElse.r
@@ -0,0 +1,13 @@
+#if else statemnets 
+a<-6
+if(a>5){
+  print("a is greater than 5")
+}else if (a==5){
+  print("a is equal to 5 ")
+}else{
+  print("a is less than 5")
+}
+B=5
+if(B>1){
+  print("B is greater than 1")
+}
